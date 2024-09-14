644f00001e2edd1f65c0c86455921c95cb4aef28
Abraham Q <abraham@cavela.com>
2024-09-14 15:32:03 -0400

    Hot reload

--- src/createGptUtterance.js
+++ src/createGptUtterance.js
@@ -1,42 +1,48 @@
-chrome.runtime.onMessage.addListener(
-    function (request, sender) {
-      if (request.message === "sendChatToGpt") {
-        chrome.storage.local.get({
-          apiChoice: 'webapp'
-          // copyChatSuggestion: 'auto',
-        }, (items) => {
-          if (items.apiChoice === 'webapp') {
-            createGPTUtteranceWebApi(request.prompt, async function (gptResponse) {
+chrome.runtime.onMessage.addListener(function (request, sender) {
+  if (request.message === "sendChatToGpt") {
+    chrome.storage.local.get(
+      {
+        apiChoice: "api",
+        // copyChatSuggestion: 'auto',
+      },
+      (items) => {
+        if (items.apiChoice === "webapp") {
+          createGPTUtteranceWebApi(
+            request.prompt,
+            async function (gptResponse) {
               //send async gptResponse to content script
               try {
                 await chrome.tabs.sendMessage(sender.tab.id, {
                   message: "gptResponse",
-                  response: gptResponse
+                  response: gptResponse,
                 });
               } catch (e) {
                 console.error("error sending message back to whatsapp");
               }
-            });
-          } else {
-            createGPTUtteranceRealApi(request.prompt, async function (gptResponse) {
+            }
+          );
+        } else {
+          createGPTUtteranceRealApi(
+            request.prompt,
+            async function (gptResponse) {
               //send async gptResponse to content script
               try {
                 await chrome.tabs.sendMessage(sender.tab.id, {
                   message: "gptResponse",
-                  response: gptResponse
+                  response: gptResponse,
                 });
               } catch (e) {
                 console.error("error sending message back to whatsapp");
               }
-            });
-          }
-        });
+            }
+          );
+        }
       }
-    }
-);
+    );
+  }
+});
 
 let chatResponseBodyReader = null;
-
 
 async function createGPTUtteranceWebApi(prompt, handleResponseOrError) {
   try {
@@ -51,8 +57,9 @@ async function createGPTUtteranceWebApi(prompt, handleResponseOrError) {
       handleResponseOrError({
         error: {
           type: "noSessionToken",
-          message: '<a href="https://chat.openai.com/" target="_blank">Click here to log in to ChatGPT</a>!'
-        }
+          message:
+            '<a href="https://chat.openai.com/" target="_blank">Click here to log in to ChatGPT</a>!',
+        },
       });
     }
   } catch (e) {
@@ -60,8 +67,8 @@ async function createGPTUtteranceWebApi(prompt, handleResponseOrError) {
     handleResponseOrError({
       error: {
         type: "unknown",
-        message: e.message
-      }
+        message: e.message,
+      },
     });
   }
 }
@@ -86,33 +93,34 @@ async function createGPTUtteranceRealApi(prompt, handleResponseOrError) {
     await chatResponseBodyReader?.cancel();
     chatResponseBodyReader?.releaseLock();
     chatResponseBodyReader = null;
-    const {apiKey} = await getValues(['apiKey']);
+    const { apiKey } = await getValues(["apiKey"]);
     if (apiKey && apiKey.length > 0) {
       await postConversationReal(apiKey, prompt, handleResponseOrError);
     } else {
       handleResponseOrError({
         error: {
           type: "noApiKey",
-          message: 'No api key found'
-        }
+          message: "No api key found",
+        },
       });
     }
-
   } catch (e) {
     console.error(e);
     handleResponseOrError({
       error: {
         type: "unknown",
-        message: e.message
-      }
+        message: e.message,
+      },
     });
   }
 }
 
 async function getSessionToken() {
-  const sessionResponse = await fetch('https://chat.openai.com/api/auth/session');
+  const sessionResponse = await fetch(
+    "https://chat.openai.com/api/auth/session"
+  );
   if (sessionResponse.status === 200) {
-    const {accessToken} = await sessionResponse.json();
+    const { accessToken } = await sessionResponse.json();
     return accessToken;
   } else {
     return null;
@@ -120,57 +128,67 @@ async function getSessionToken() {
 }
 
 function generateUUIDv4() {
-  return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
-      (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+  return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
+    (
+      c ^
+      (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
+    ).toString(16)
   );
 }
 
 function createHeaders(sessionToken) {
   return new Headers({
-    'Content-Type': 'application/json',
-    'Authorization': 'Bearer ' + sessionToken,
+    "Content-Type": "application/json",
+    Authorization: "Bearer " + sessionToken,
   });
 }
 
 async function postConversation(sessionToken, prompt, handleResponseOrError) {
   const requestPayload = {
-    action: 'next',
+    action: "next",
     messages: [
       {
         id: generateUUIDv4(),
-        role: 'user',
+        role: "user",
         content: {
-          content_type: 'text',
+          content_type: "text",
           parts: [prompt],
         },
       },
     ],
-    model: 'text-davinci-002-render-sha',
+    model: "text-davinci-002-render-sha",
     parent_message_id: generateUUIDv4(),
-    is_visible: false
-  }
+    is_visible: false,
+  };
 
-  const chatResponse = await fetch('https://chat.openai.com/backend-api/conversation', {
-    method: 'POST',
-    headers: createHeaders(sessionToken),
-    body: JSON.stringify(requestPayload),
-  });
+  const chatResponse = await fetch(
+    "https://chat.openai.com/backend-api/conversation",
+    {
+      method: "POST",
+      headers: createHeaders(sessionToken),
+      body: JSON.stringify(requestPayload),
+    }
+  );
   if (!chatResponse.ok) {
     switch (chatResponse.status) {
       case 429:
         handleResponseOrError({
           error: {
             type: "processingError",
-            message: "You have been rate-limited by ChatGPT. Please try again."
-          }
+            message: "You have been rate-limited by ChatGPT. Please try again.",
+          },
         });
         break;
-      default :
+      default:
         handleResponseOrError({
           error: {
             type: "processingError",
-            message: "Something went wrong fetching the answer from GPT; Statuscode: " + chatResponse.status + "," + chatResponse.statusText
-          }
+            message:
+              "Something went wrong fetching the answer from GPT; Statuscode: " +
+              chatResponse.status +
+              "," +
+              chatResponse.statusText,
+          },
         });
     }
     return;
@@ -179,24 +197,23 @@ async function postConversation(sessionToken, prompt, handleResponseOrError) {
   try {
     chatResponseBodyReader = chatResponse.body.getReader();
     while (true) {
-      const {done, value} = await chatResponseBodyReader.read();
+      const { done, value } = await chatResponseBodyReader.read();
       if (done) {
         break;
       }
       const streamOutput = new TextDecoder().decode(value);
-      const lastAssistantContent = getLastAssistantContentMessage(streamOutput)
+      const lastAssistantContent = getLastAssistantContentMessage(streamOutput);
       if (lastAssistantContent) {
-        handleResponseOrError({text: lastAssistantContent})
+        handleResponseOrError({ text: lastAssistantContent });
       }
     }
   } catch (e) {
     console.error(e);
     handleResponseOrError({
-      error:
-          {
-            type: "unknown",
-            message: e.message
-          }
+      error: {
+        type: "unknown",
+        message: e.message,
+      },
     });
   } finally {
   }
@@ -205,34 +222,41 @@ async function postConversation(sessionToken, prompt, handleResponseOrError) {
 async function postConversationReal(apiKey, prompt, handleResponseOrError) {
   const requestPayload = {
     model: "gpt-3.5-turbo",
-    messages: [{role: "user", content: prompt}],
+    messages: [{ role: "user", content: prompt }],
     // max_tokens: 4096,
     temperature: 0.5,
     // frequency_penalty: 1.0,
     // stream: true,
   };
 
-  const chatResponse = await fetch('https://api.openai.com/v1/chat/completions', {
-    method: 'POST',
-    headers: createHeaders(apiKey),
-    body: JSON.stringify(requestPayload),
-  });
+  const chatResponse = await fetch(
+    "https://api.openai.com/v1/chat/completions",
+    {
+      method: "POST",
+      headers: createHeaders(apiKey),
+      body: JSON.stringify(requestPayload),
+    }
+  );
   if (!chatResponse.ok) {
     switch (chatResponse.status) {
       case 429:
         handleResponseOrError({
           error: {
             type: "processingError",
-            message: "You have been rate-limited by ChatGPT. Please try again."
-          }
+            message: "You have been rate-limited by ChatGPT. Please try again.",
+          },
         });
         break;
-      default :
+      default:
         handleResponseOrError({
           error: {
             type: "processingError",
-            message: "Something went wrong fetching the answer from GPT; Statuscode: " + chatResponse.status + "," + chatResponse.statusText
-          }
+            message:
+              "Something went wrong fetching the answer from GPT; Statuscode: " +
+              chatResponse.status +
+              "," +
+              chatResponse.statusText,
+          },
         });
     }
     return;
@@ -241,7 +265,7 @@ async function postConversationReal(apiKey, prompt, handleResponseOrError) {
   try {
     chatResponseBodyReader = chatResponse.body.getReader();
     while (true) {
-      const {done, value} = await chatResponseBodyReader.read();
+      const { done, value } = await chatResponseBodyReader.read();
       if (done) {
         break;
       }
@@ -250,7 +274,7 @@ async function postConversationReal(apiKey, prompt, handleResponseOrError) {
       let content = null;
       if (undefined !== chatGetAnswer.error) {
         let message = chatGetAnswer.error.message;
-        console.log("chatGetAnswerError:", message)
+        console.log("chatGetAnswerError:", message);
         content = message;
       } else {
         try {
@@ -261,17 +285,16 @@ async function postConversationReal(apiKey, prompt, handleResponseOrError) {
       }
 
       if (content) {
-        handleResponseOrError({text: content})
+        handleResponseOrError({ text: content });
       }
     }
   } catch (e) {
     console.error(e);
     handleResponseOrError({
-      error:
-          {
-            type: "unknown",
-            message: e.message
-          }
+      error: {
+        type: "unknown",
+        message: e.message,
+      },
     });
   } finally {
   }
@@ -280,15 +303,15 @@ async function postConversationReal(apiKey, prompt, handleResponseOrError) {
 function getLastAssistantContentMessage(input) {
   const regex = /"role":\s*"assistant".+?"parts":\s*\[(.*?)\]/g;
   let match;
-  let lastAssistantContent = '';
+  let lastAssistantContent = "";
 
-  match = regex.exec(input)
+  match = regex.exec(input);
   if (!match) {
     return null;
   }
   while (match !== null) {
     lastAssistantContent = match[1];
-    match = regex.exec(input)
+    match = regex.exec(input);
   }
   return JSON.parse(lastAssistantContent);
 }
